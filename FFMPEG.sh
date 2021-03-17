ad906a517812c12264afaf5e26771b33227febd4
FlareFlo <florianlipp17@gmail.com>
2021-03-17 22:01:17 +0100

    Reworked config system

--- FFMPEG.sh
+++ FFMPEG.sh
@@ -1,1 +1,1 @@
-ffmpeg -i ${1} -b:a 96k -c:v libx264 -vf scale=1280:720 -preset slow -fs "$(jq ".configs[0] | .size"  test.json)" ${1}compressed-8mb.mp4
+ffmpeg -i "${1}" -b:a "$(jq ".configs[0] | .audiobitrate" config.json)" -c:v libx264 -vf scale=1280:720 -preset slow -crf "$(jq ".configs[0] | .quality" config.json)" -fs "$(jq ".configs[0] | .size"  config.json)" "${1}"compressed-8mb.mp4
