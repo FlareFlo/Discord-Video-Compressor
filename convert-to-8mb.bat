bead155a75522b4cc8e325d07f6342522d08d6ec
FlareFlo <florianlipp17@gmail.com>
2021-03-17 13:09:14 +0100

    Improvements to the 8mb script accuracy
    Additionally added comments to the batch files

--- convert-to-8mb.bat
+++ convert-to-8mb.bat
@@ -1,5 +1,7 @@
-cd %~dp0
-set X=%~dp0
-set X=%X:\=/%
-set X=%X:C:=/mnt/c%
+@echo off
+cd %~dp0 									&:: Sets current directory
+set X=%~dp0									&:: Initializes varaible X with currenty directory
+set drive=%CD:~0,1%							&:: Initializes variable drive with the current drive letter as such: "c"§
+set X=%X:\=/%								&:: Replaces all \ with / in X path compliant formatting
+set X=%X:C:=mnt/c%							&:: Replaces the C: in X with mnt/c for WSL compliant formatting
 cmd /k wsl %X%/script8mb "%~n1%~x1"
