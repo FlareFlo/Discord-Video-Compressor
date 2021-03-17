4b1cb7253b65e11eea147689e8f8c9b2196a013a
FlareFlo <florianlipp17@gmail.com>
2021-03-17 13:37:33 +0100

    Reverted commenting, as batch really dislikes it

--- convert-to-8mb.bat
+++ convert-to-8mb.bat
@@ -1,7 +1,5 @@
-@echo off
-cd %~dp0 									&:: Sets current directory
-set X=%~dp0									&:: Initializes varaible X with currenty directory
-set drive=%CD:~0,1%							&:: Initializes variable drive with the current drive letter as such: "c"§
-set X=%X:\=/%								&:: Replaces all \ with / in X path compliant formatting
-set X=%X:C:=mnt/c%							&:: Replaces the C: in X with mnt/c for WSL compliant formatting
+cd %~dp0
+set X=%~dp0
+set X=%X:\=/%
+set X=%X:C:=/mnt/c%
 cmd /k wsl %X%/script8mb "%~n1%~x1"
