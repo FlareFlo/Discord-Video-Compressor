2178dce924a2c2d67938231ef0d8b07305a818b2
FlareFlo <florianlipp17@gmail.com>
2021-03-21 17:14:47 +0100

    Full config support working
    Signed-off-by: FlareFlo <florianlipp17@gmail.com>

--- FFMPEG.sh
+++ FFMPEG.sh
@@ -2,4 +2,4 @@ audiobitrate=$(jq -r ".configs[0] | .audiobitrate" $(pwd)/$(jq -r ".configs[0] |
 horizontalresolution="$(jq ".configs[0] | .horizontalresolution" $(pwd)/$(jq -r ".configs[0] | .selection" main-config.json))"
 verticalresolution="$(jq ".configs[0] | .verticalresolution" $(pwd)/$(jq -r ".configs[0] | .selection" main-config.json))"
 crf="$(jq ".configs[0] | .quality" $(pwd)/$(jq -r ".configs[0] | .selection" main-config.json))"
-ffmpeg -i "${1}" -b:a "$audiobitrate" -c:v libx264 -vf scale="$horizontalresolution":"$verticalresolution" -preset "$(jq -r ".configs[0] | .preset" $config)" -crf "$crf" -fs "$(jq -r ".configs[0] | .size" $config)" "${1}"compressed.mp4
+ffmpeg -i "${1}" -b:a "$audiobitrate" -c:v libx264 -vf scale="$horizontalresolution":"$verticalresolution" -preset "$(jq -r ".configs[0] | .preset" $(pwd)/$(jq -r ".configs[0] | .selection" main-config.json))" -crf "$crf" -fs "$(jq -r ".configs[0] | .size" $(pwd)/$(jq -r ".configs[0] | .selection" main-config.json))" "${1}"compressed.mp4
