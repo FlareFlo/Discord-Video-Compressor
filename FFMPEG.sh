2e4102c46522fba787f17e41b7d53ad281bac7c4
FlareFlo <florianlipp17@gmail.com>
2021-03-22 15:02:27 +0100

    Changed output formatting from json
    Signed-off-by: FlareFlo <florianlipp17@gmail.com>

--- FFMPEG.sh
+++ FFMPEG.sh
@@ -1,4 +1,4 @@
-audiobitrate=$(jq -r ".configs[0] | .audiobitrate" $(pwd)/$(jq -r ".configs[0] | .selection" main-config.json))
-resolution="$(jq -r ".configs[0] | .resolution" $(pwd)/$(jq -r ".configs[0] | .selection" main-config.json))"
-crf="$(jq -r ".configs[0] | .quality" $(pwd)/$(jq -r ".configs[0] | .selection" main-config.json))"
-ffmpeg -i "${1}" $audiobitrate -c:v libx264 $resolution  -preset "$(jq -r ".configs[0] | .preset" $(pwd)/$(jq -r ".configs[0] | .selection" main-config.json))" -crf "$crf" -fs "$(jq -r ".configs[0] | .size" $(pwd)/$(jq -r ".configs[0] | .selection" main-config.json))" "${1}"compressed.mp4
+audiobitrate=$(jq -j ".configs[0] | .audiobitrate" $(pwd)/$(jq -j ".configs[0] | .selection" main-config.json))
+resolution="$(jq -j ".configs[0] | .resolution" $(pwd)/$(jq -j ".configs[0] | .selection" main-config.json))"
+crf="$(jq -j ".configs[0] | .quality" $(pwd)/$(jq -j ".configs[0] | .selection" main-config.json))"
+ffmpeg -i "${1}" $audiobitrate -c:v libx264 $resolution  -preset "$(jq -r ".configs[0] | .preset" $(pwd)/$(jq -j ".configs[0] | .selection" main-config.json))" -crf "$crf" -fs "$(jq -j ".configs[0] | .size" $(pwd)/$(jq -j ".configs[0] | .selection" main-config.json))" "${1}"compressed.mp4
