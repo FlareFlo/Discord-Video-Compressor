90dcb326c7ba9e32f046b20d75c00b3667c032fb
FlareFlo <florianlipp17@gmail.com>
2021-03-22 14:32:18 +0100

    Config optimization
    Signed-off-by: FlareFlo <florianlipp17@gmail.com>

--- FFMPEG.sh
+++ FFMPEG.sh
@@ -1,5 +1,5 @@
 audiobitrate=$(jq -r ".configs[0] | .audiobitrate" $(pwd)/$(jq -r ".configs[0] | .selection" main-config.json))
 horizontalresolution="$(jq ".configs[0] | .horizontalresolution" $(pwd)/$(jq -r ".configs[0] | .selection" main-config.json))"
 verticalresolution="$(jq ".configs[0] | .verticalresolution" $(pwd)/$(jq -r ".configs[0] | .selection" main-config.json))"
-crf="$(jq ".configs[0] | .quality" $(pwd)/$(jq -r ".configs[0] | .selection" main-config.json))"
-ffmpeg -i "${1}" -b:a "$audiobitrate" -c:v libx264 -vf scale="$horizontalresolution":"$verticalresolution" -preset "$(jq -r ".configs[0] | .preset" $(pwd)/$(jq -r ".configs[0] | .selection" main-config.json))" -crf "$crf" -fs "$(jq -r ".configs[0] | .size" $(pwd)/$(jq -r ".configs[0] | .selection" main-config.json))" "${1}"compressed.mp4
+crf="$(jq -r ".configs[0] | .quality" $(pwd)/$(jq -r ".configs[0] | .selection" main-config.json))"
+ffmpeg -i "${1}" $audiobitrate -c:v libx264 -vf scale="$horizontalresolution":"$verticalresolution" -preset "$(jq -r ".configs[0] | .preset" $(pwd)/$(jq -r ".configs[0] | .selection" main-config.json))" -crf "$crf" -fs "$(jq -r ".configs[0] | .size" $(pwd)/$(jq -r ".configs[0] | .selection" main-config.json))" "${1}"compressed.mp4
